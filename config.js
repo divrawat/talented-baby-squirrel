56fb2d0b6212a820a44b6d42f16cf3d47c9eced1
divrawat <divrawat2001@gmail>
2024-07-02 22:23:48 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,447 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.talentedbabysquirrel.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>Talented Baby Squirrel</u></a> is a fantasy romance webcomic. The story follows a character who reincarnates as a squirrel shifter and gets a second chance at life.  This time, they're welcomed by their biological family and possess a hidden talent waiting to be discovered.`;
+
+export const MANGA_NAME = 'Talented Baby Squirrel';
+export const MANGA_AUTHOR = 'Not Known';
+export const MANGA_RELEASE = '2023';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Not Known';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Fantasy, Drama, Thriller, Romance';
+
+export const CHAPTER_PREFIX = 'talented-baby-squirrel-chapter';
+export const IMAGE_PREFIX = 'images/talented-baby-squirrel';
+export const NEXT_PREVIOUS_PREFIX = 'manga/talented-baby-squirrel-chapter';
+export const URL_PREFIX = 'manga/talented-baby-squirrel-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/talented-baby-squirrel-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://cdn.imgchest.com/files/3yrgckxv6n4.png";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[75px] rounded-full mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Talented Baby Squirrel';
+export const NavbarName = "Talented Baby Squirrel";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. It is a fantasy romance webcomic. The story follows a character who reincarnates as a squirrel shifter and gets a second chance at life.  This time, they're welcomed by their biological family and possess a hidden talent waiting to be discovered.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "Talented Baby Squirrel is a heartwarming tale that follows the adventures of a young protagonist, a baby squirrel born with extraordinary abilities and a charming personality. Set in a vibrant woodland setting, the novel captures the innocence and curiosity of youth as the baby squirrel embarks on a journey of self-discovery and growth."
+    },
+    {
+        id: 2,
+        content: "From the outset, the story paints a vivid picture of the squirrel's natural talents, which set them apart from their peers in the forest. Whether it's their agility in climbing trees, keen sense of smell for finding food, or their ability to communicate with other woodland creatures, the protagonist quickly becomes a beloved figure in their community."
+    },
+    {
+        id: 3,
+        content: "Central to the narrative is the protagonist's relationships with other forest inhabitants. They forge deep bonds with fellow animals, from wise old owls to playful rabbits and mischievous raccoons, each offering unique perspectives and life lessons along the way. These interactions highlight themes of friendship, cooperation, and empathy, as the baby squirrel learns valuable lessons about trust and mutual support."
+    },
+    {
+        id: 4,
+        content: "As the story unfolds, the protagonist encounters challenges that test their courage and resilience. From navigating through harsh weather conditions to encountering natural predators, each obstacle serves as a stepping stone for personal growth and development. Through determination and resourcefulness, the baby squirrel overcomes adversity, gaining confidence in their abilities and earning the respect of their forest companions."
+    },
+    {
+        id: 5,
+        content: "Beyond the physical trials, Talented Baby Squirrel also explores emotional and moral dilemmas faced by the protagonist. They confront issues such as fairness, forgiveness, and the importance of protecting their natural habitat. These themes resonate throughout the narrative, offering readers a deeper understanding of environmental stewardship and the interconnectedness of all living beings."
+    },
+    {
+        id: 6,
+        content: "Ultimately, Talented Baby Squirrel is a celebration of curiosity, resilience, and the transformative power of friendship. It invites readers of all ages to embark on an enchanting journey through the eyes of a charismatic young squirrel, whose adventures illuminate the beauty of nature and the universal values that bind us together. Through its engaging storyline and lovable characters, the novel leaves a lasting impression, inspiring empathy and appreciation for the wonders of the natural world."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 48
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "2a",
+        "numImages": 45
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 42
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 3
+    },
+    {
+        "chapterNumber": "34a",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "62",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "63",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "64",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "65",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "66",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "67",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "68",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "69",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "70",
+        "numImages": 46
+    },
+    {
+        "chapterNumber": "71",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "72",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "73",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "74",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "75",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "76",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "77",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "78",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "79",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "80",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "81",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "82",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "83",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "84",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "85",
+        "numImages": 17
+    }
+]
